d9c03373b6d2e1d5dd54e56197a7e924ba86021b
ANTOINE_NAME TUJAS_NAME <antoine.tujas33@gmail.com>
2023-07-31 22:19:54 +0200

    exo 3 fini

--- userHandlers.js
+++ userHandlers.js
@@ -1,32 +1,63 @@
 const users = [
   {
     id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
+    firstname: "John",
+    lastname: "Doe",
+    email: "john.doe@example.com",
+    city: "Paris",
+    language: "English",
   },
   {
     id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
+    firstname: "Valeriy",
+    lastname: "Appius",
+    email: "valeriy.appius@example.com",
+    city: "Moscow",
+    language: "Russian",
   },
   {
     id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
+    firstname: "Ralf",
+    lastname: "Geronimo",
+    email: "ralf.geronimo@example.com",
+    city: "New York",
+    language: "Italian",
   },
 ];
 
+const database = require("./database");
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 const getUsers = (req, res) => {
-  res.status(200).json(users);
+  database
+
+    .query("select * from users")
+
+    .then(([users]) => {
+      res.json(users);
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getUserById = (req, res) => {
@@ -44,4 +75,5 @@ const getUserById = (req, res) => {
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
 };
