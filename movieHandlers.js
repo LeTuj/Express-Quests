a08cd7d7d027233f4fb188175365aca3807679c3
ANTOINE_NAME TUJAS_NAME <antoine.tujas33@gmail.com>
2023-08-01 15:54:30 +0200

    exo 5 fini

--- movieHandlers.js
+++ movieHandlers.js
@@ -27,6 +27,24 @@ const movies = [
 
 const database = require("./database");
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
 const updateMovie = (req, res) => {
   const id = parseInt(req.params.id);
   const { title, director, year, color, duration } = req.body;
@@ -101,4 +119,5 @@ module.exports = {
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie,
 };
